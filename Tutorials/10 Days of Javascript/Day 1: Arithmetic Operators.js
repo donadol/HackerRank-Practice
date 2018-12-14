426c617ac05f52530ebe9449b77271c142f3bc56
Laura Donado <ldonado11@gmail.com>
2018-12-14 10:36:51 -0500

    Create Day 1: Arithmetic Operators.js

---
+++ Tutorials/10 Days of Javascript/Day 1: Arithmetic Operators.js
@@ -0,0 +1,38 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    
+    return length*width;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    
+    return 2*(length+width);
+}
+
+
+function main() {
+    const length = +(readLine());
+    const width = +(readLine());
+    
+    console.log(getArea(length, width));
+    console.log(getPerimeter(length, width));
+}
