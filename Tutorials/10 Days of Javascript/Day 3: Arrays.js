2d2a1b81c82bb0e915ec8141881013f89e0c529a
Laura Donado <ldonado11@gmail.com>
2018-12-14 10:40:42 -0500

    Create Day 3: Arrays.js

---
+++ Tutorials/10 Days of Javascript/Day 3: Arrays.js
@@ -0,0 +1,22 @@
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    nums.sort((a, b) => b - a);
+    let a=nums[0];
+    for (let e of nums) {
+      if(e!=a)
+        return e;
+    }
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
