b3e2e2a587b6c41e3dab1629b3e2349a5b3a3884
Laura Donado <ldonado11@gmail.com>
2018-12-14 10:35:52 -0500

    Update Day 0: Data Types.js

--- Tutorials/10 Days of Javascript/Day 0: Data Types.js
+++ Tutorials/10 Days of Javascript/Day 0: Data Types.js
@@ -31,3 +31,10 @@ function performOperation(secondInteger, secondDecimal, secondString) {
 }
 
 
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+    
+    performOperation(secondInteger, secondDecimal, secondString);
+}
